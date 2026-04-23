7c79dc4e46480262ad41bd24ee923346d818e68b
DatingMoldova <pokerdendi2@gmail.com>
2026-04-23 20:52:02 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,5 +4,5 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 REQUIRED_CHANNEL = int(os.getenv("REQUIRED_CHANNEL"))
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL"))
-
+CHANNEL_LINK = os.getenv("CHANNEL_LINK")
 BOT_USERNAME = os.getenv("BOT_USERNAME")
