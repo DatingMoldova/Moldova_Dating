38839de33e74caf3e362613f72d7f36a6da9a6a1
DatingMoldova <pokerdendi2@gmail.com>
2026-04-24 02:41:10 +0300

    Update loader.py

--- bot/loader.py
+++ bot/loader.py
@@ -6,7 +6,7 @@ from bot.handlers.register import router as register_router
 from bot.handlers.profile import router as profile_router
 from bot.handlers.agreement import router as agreement_router
 from bot.handlers.menu import router as menu_router
-
+from bot.handlers.about import router as about_router
 
 def setup_routers(dp: Dispatcher):
     # 🔥 порядок важен
@@ -15,5 +15,5 @@ def setup_routers(dp: Dispatcher):
     dp.include_router(profile_router)
 
     dp.include_router(agreement_router)
-
+    dp.include_router(about_router)
     dp.include_router(menu_router)
