e64765b83caced30ced12a3bb31bc5b89b0a5af4
DatingMoldova <pokerdendi2@gmail.com>
2026-04-24 02:42:18 +0300

    Delete bot/keyboards/agree_kb.py

--- bot/keyboards/agree_kb.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-
-def agree_kb():
-    return InlineKeyboardMarkup(
-        inline_keyboard=[
-            [InlineKeyboardButton(text="⬇️ Свернуть", callback_data="hide_agreement")]
-        ]
-    )
