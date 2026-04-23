3f6b3fa6028d5544647d61d57b8b1ae2831487fb
DatingMoldova <pokerdendi2@gmail.com>
2026-04-23 19:53:28 +0300

    Update main_menu.py

--- bot/keyboards/main_menu.py
+++ bot/keyboards/main_menu.py
@@ -1,1 +1,10 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+main_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="👤 Моя анкета")],
+        [KeyboardButton(text="💬 Наш чат")],
+        [KeyboardButton(text="🔍 Поиск (в разработке)")]
+    ],
+    resize_keyboard=True
+)
