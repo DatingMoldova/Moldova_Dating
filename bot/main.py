f38e9d6185850eacfcc639d826d1f1048c395c94
DatingMoldova <pokerdendi2@gmail.com>
2026-04-24 02:36:06 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -7,31 +7,28 @@ from aiogram.client.default import DefaultBotProperties
 
 from bot.config import BOT_TOKEN
 from bot.db import init_db
-
-from bot.handlers.start import router as start_router
-from bot.handlers.register import router as register_router
-from bot.handlers.menu import router as menu_router
-from bot.handlers.profile import router as profile_router
+from bot.loader import setup_routers
 
 
 async def main():
     logging.basicConfig(level=logging.INFO)
 
+    # 🔥 БД
     init_db()
 
+    # 🔥 БОТ
     bot = Bot(
         token=BOT_TOKEN,
         default=DefaultBotProperties(parse_mode=ParseMode.HTML)
     )
 
+    # 🔥 ДИСПЕТЧЕР
     dp = Dispatcher()
 
-    # 🔥 ВАЖНЫЙ ПОРЯДОК
-    dp.include_router(start_router)
-    dp.include_router(register_router)
-    dp.include_router(profile_router)  # 👈 ДО menu
-    dp.include_router(menu_router)
+    # 🔥 ПОДКЛЮЧЕНИЕ ВСЕГО
+    setup_routers(dp)
 
+    # 🔥 фикс зависаний
     await bot.delete_webhook(drop_pending_updates=True)
 
     print("🚀 Бот запущен")
