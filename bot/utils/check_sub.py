894a89e474edb83151795be11c9bd2c949102843
DatingMoldova <pokerdendi2@gmail.com>
2026-04-23 19:52:59 +0300

    Update check_sub.py

--- bot/utils/check_sub.py
+++ bot/utils/check_sub.py
@@ -1,1 +1,6 @@
-
+async def check_subscription(bot, user_id, channel_id):
+    try:
+        member = await bot.get_chat_member(channel_id, user_id)
+        return member.status in ["member", "administrator", "creator"]
+    except:
+        return False
