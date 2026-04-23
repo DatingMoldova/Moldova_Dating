beeff440c4a6de07a5971bcbb385338b7abbb1d5
DatingMoldova <pokerdendi2@gmail.com>
2026-04-23 22:36:51 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -7,7 +7,6 @@ from aiogram.enums import ParseMode
 from bot.config import BOT_TOKEN
 from bot.db import init_db
 
-# handlers
 from bot.handlers.start import router as start_router
 from bot.handlers.register import router as register_router
 from bot.handlers.menu import router as menu_router
@@ -16,21 +15,17 @@ from bot.handlers.menu import router as menu_router
 async def main():
     logging.basicConfig(level=logging.INFO)
 
-    # Инициализация БД
     init_db()
 
-    # Бот
     bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
     dp = Dispatcher()
 
-    # Роутеры
     dp.include_router(start_router)
     dp.include_router(register_router)
     dp.include_router(menu_router)
 
     print("🚀 Бот запущен")
 
-    # Запуск
     await dp.start_polling(bot)
 
 
