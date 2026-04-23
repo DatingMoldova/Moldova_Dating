0b77a14dc8e0f757707a0468235a52556f577f42
DatingMoldova <pokerdendi2@gmail.com>
2026-04-24 02:35:24 +0300

    Create loader.py

---
+++ bot/loader.py
@@ -0,0 +1,19 @@
+from aiogram import Dispatcher
+
+# 🔥 все роутеры
+from bot.handlers.start import router as start_router
+from bot.handlers.register import router as register_router
+from bot.handlers.profile import router as profile_router
+from bot.handlers.agreement import router as agreement_router
+from bot.handlers.menu import router as menu_router
+
+
+def setup_routers(dp: Dispatcher):
+    # 🔥 порядок важен
+    dp.include_router(start_router)
+    dp.include_router(register_router)
+    dp.include_router(profile_router)
+
+    dp.include_router(agreement_router)
+
+    dp.include_router(menu_router)
