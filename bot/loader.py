c56e22c85f9958b25ee263d2224b7570a1e4374a
DatingMoldova <pokerdendi2@gmail.com>
2026-04-24 11:58:02 +0300

    Update loader.py

--- bot/loader.py
+++ bot/loader.py
@@ -1,10 +1,6 @@
-from bot.handlers.start import router as start_router
-from bot.handlers.register import router as register_router
-from bot.handlers.profile import router as profile_router
-from bot.handlers.admin import router as admin_router
+from bot.handlers import register, profile, admin
 
 def setup_routers(dp):
-    dp.include_router(start_router)
-    dp.include_router(register_router)
-    dp.include_router(profile_router)
-    dp.include_router(admin_router)
+    dp.include_router(register.router)
+    dp.include_router(profile.router)
+    dp.include_router(admin.router)
