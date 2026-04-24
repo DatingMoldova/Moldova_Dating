8a2847dbf51dece74ba9cdd06e2190eb00fd3aaf
DatingMoldova <pokerdendi2@gmail.com>
2026-04-24 03:10:46 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -9,5 +9,8 @@ CHANNEL_LINK = os.getenv("CHANNEL_LINK")
 
 BOT_USERNAME = os.getenv("BOT_USERNAME")
 
+LOG_CHAT_ID = int(os.getenv("LOG_CHAT_ID"))
+LOG_THREAD_ID = int(os.getenv("LOG_THREAD_ID"))
+
 # 🔥 админ через Railway
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
