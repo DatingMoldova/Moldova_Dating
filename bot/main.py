9b8df693a9a2e06fc554ad2f801f65b8d9dc34f3
DatingMoldova <pokerdendi2@gmail.com>
2026-04-24 11:17:39 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,16 +1,23 @@
 import asyncio
+import logging
 from aiogram import Bot, Dispatcher
 from bot.loader import setup_routers
 from bot.config import TOKEN
 
+logging.basicConfig(level=logging.INFO)
+
 async def main():
-    bot = Bot(token=TOKEN, parse_mode="HTML")
-    dp = Dispatcher()
+    try:
+        bot = Bot(token=TOKEN, parse_mode="HTML")
+        dp = Dispatcher()
 
-    setup_routers(dp)
+        setup_routers(dp)
 
-    print("🚀 Bot started")
-    await dp.start_polling(bot)
+        print("🚀 Bot started")
+        await dp.start_polling(bot)
+
+    except Exception as e:
+        print("💥 ERROR:", e)
 
 if __name__ == "__main__":
     asyncio.run(main())
